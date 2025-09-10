56066c108b89390e0fe340194a310acf337556a2
202330124 <202330124@email.daelim.ac.kr>
2025-09-10 12:33:22 +0900

    로딩 컴포넌트 추가

--- src/app/(marketing)/blog/page.tsx
+++ src/app/(marketing)/blog/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
-export default function Blog() {
+export default async function Blog() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   return (
     <h1>This is Blog Page</h1>
   );
